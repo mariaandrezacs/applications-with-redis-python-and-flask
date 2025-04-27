97ada1038026667105c5c85cb7bdf3f399c85ba3
Maria Andreza C Santos <mandreza008@gmail.com>
2025-04-26 21:13:14 -0300

    test: comandos para hash

--- src/redis_raw.py
+++ src/redis_raw.py
@@ -8,8 +8,34 @@ redis_conn.set("chave_1", "trocando_o_meu_valor")
 
 # select
 meu_valor = redis_conn.get("chave_1").decode("utf-8")
-print(meu_valor)
-print(type(meu_valor))
+# print(meu_valor)
+# print(type(meu_valor))
 
 # delete
 redis_conn.delete("chave_1")
+
+
+### comandos para hash
+meu_hash = {    # chave de exemplo
+    "nome": "joao", # field: values
+    "idade": 30,
+    "cidade": "sao paulo"
+}
+# insert and update
+redis_conn.hset("meu_hash", "nome", "joao")
+redis_conn.hset("meu_hash", "idade", "idade")
+redis_conn.hset("meu_hash", "cidade", "curitiba")
+
+# select
+valor_1 = redis_conn.hget("meu_hash", "nome").decode("utf-8")
+# print(valor_1)
+
+redis_conn.hdel("meu_hash", "cidade")
+
+
+### buscas por existencia
+elem = redis_conn.exists("chave_1")
+print(elem)
+
+elem_2 = redis_conn.hexists("meu_hash", "nome")
+print(elem_2)
