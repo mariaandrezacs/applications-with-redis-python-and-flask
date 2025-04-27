4b07f2a45df7143cbb21578881bb50a8747c1d7c
Maria Andreza C Santos <mandreza008@gmail.com>
2025-04-27 14:54:42 -0300

    tests: implementa tests no repositorio do sqlite

---
+++ src/models/sqlite/repository/products_repository.py
@@ -0,0 +1,28 @@
+from sqlite3 import Connection as SqliteConnection
+
+
+class ProductsRepository:
+    def __init__(self, conn: SqliteConnection) -> None:
+        self.__conn = conn
+
+    def fin_product_by_name(self, product_name: str) -> tuple:
+        cursor = self.__conn.cursor()
+        cursor.execute(
+            "SELECT * FROM products WHERE name = ?",
+            (product_name, )
+        )
+        product = cursor.fetchone()
+        return product
+
+    def insert_product(self, name: str, prince: float, quantity: int) -> None:
+        cursor = self.__conn.cursor()
+        cursor.execute(
+            """
+                INSERT INTO products
+                    (name, price, quantity)
+                VALUES
+                    (?, ?, ?)
+            """,
+            (name, prince, quantity)
+        )
+        self.__conn.commit()
